3696df7fc1713790232f63464d4175b705bec47d
Matt Bentley <mbentley@mbentley.net>
2020-06-16 13:07:42 -0400

    Added fallback for lsof not working on hosts without lsof
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM ubuntu:18.04
 
+RUN apt-get update &&\
+  apt-get install -y lsof &&\
+  rm -rf /var/lib/apt/lists/*
+
 COPY script.sh /script.sh
 
 CMD ["/script.sh"]
--- script.sh
+++ script.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-nsenter -t 1 -m -u -n -i bash -c 'echo -e "FILES\tPID\tPROCESS"; lsof -nP 2>/dev/null | awk '"'"'{ print $2; }'"'"' | sort -rn | uniq -c | sort -rn | head -5 | awk '"'"'{print $1 " " $2}'"'"' | while IFS= read -r LINE; do   PID="$(echo "$LINE" | awk '"'"'{print $2}'"'"')";   echo -e "$(echo "$LINE" | awk '"'"'{print $1}'"'"')\t${PID}\t$(ps -fp "${PID}" -o cmd=)"; done'
+# check to see if lsof exists in the host namespace
+if [ "$(nsenter -t 1 -u -m -n -i lsof -v 2> /dev/null; echo $?)" = "0" ]
+then
+  echo "OK: lsof found on the host"
+  # lsof exists in the host namespace; use it because of accuracy
+  nsenter -t 1 -m -u -n -i bash -c 'echo -e "FILES\tPID\tPROCESS"; lsof -nP 2>/dev/null | awk '"'"'{ print $2; }'"'"' | sort -rn | uniq -c | sort -rn | head -5 | awk '"'"'{print $1 " " $2}'"'"' | while IFS= read -r LINE; do PID="$(echo "$LINE" | awk '"'"'{print $2}'"'"')"; echo -e "$(echo "$LINE" | awk '"'"'{print $1}'"'"')\t${PID}\t$(ps -fp "${PID}" -o cmd=)"; done'
+else
+  # lsof does not exist in the host namespace; use it from the container
+  echo "WARNING: lsof not found on the host; results may not be 100% accurate"
+  nsenter -t 1 -u -n -i bash -c 'echo -e "FILES\tPID\tPROCESS"; lsof -nP 2>/dev/null | awk '"'"'{ print $2; }'"'"' | sort -rn | uniq -c | sort -rn | head -5 | awk '"'"'{print $1 " " $2}'"'"' | while IFS= read -r LINE; do PID="$(echo "$LINE" | awk '"'"'{print $2}'"'"')"; echo -e "$(echo "$LINE" | awk '"'"'{print $1}'"'"')\t${PID}\t$(ps -fp "${PID}" -o cmd=)"; done'
+fi
